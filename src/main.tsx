0223345a02023d835a05cdb42390718b64c811d7
Craig P. Motlin <cmotlin@gmail.com>
2024-11-01 12:29:58 -0400

    Turn on StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -20,6 +20,8 @@ const rootElement = document.getElementById('app')!
 if (!rootElement.innerHTML) {
   const root = createRoot(rootElement)
   root.render(
+    <StrictMode>
     <RouterProvider router={router} />
+    </StrictMode>
   )
 }
