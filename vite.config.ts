38a294a8f46735e842dbcf58d638d766a71a57bd
Craig P. Motlin <cmotlin@gmail.com>
2024-11-04 15:53:23 -0500

    Fix build errors about missing tanstack router ./src/routeTree.gen.ts file.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import preact from '@preact/preset-vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-    plugins: [preact(), TanStackRouterVite()],
+    plugins: [
+        preact(),
+        TanStackRouterVite({
+            generatedRouteTree: './src/routeTree.gen.ts', // Specify the output path
+            routesDirectory: './src/routes',              // Specify the routes directory
+        }),
+    ],
     build: {
         target: 'esnext',
     }
