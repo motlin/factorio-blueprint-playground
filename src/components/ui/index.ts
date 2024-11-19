a342fa1ca53cd08a3670fcbcb8db9de04241a27d
Craig P. Motlin <cmotlin@gmail.com>
2024-11-18 21:49:51 -0500

    Delete unused component CollapsiblePanel.tsx

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,6 @@
 export { Panel } from './Panel';
 export { Button } from './Button';
 export { Textarea } from './Textarea';
-export { CollapsiblePanel } from './CollapsiblePanel';
 export { ButtonGreen } from './ButtonGreen';
 export { InsetLight } from './InsetLight';
 export { InsetDark } from './InsetDark';
@@ -9,4 +8,3 @@ export { ErrorAlert } from './ErrorAlert';
 
 export type { PanelProps } from './Panel';
 export type { TextareaProps } from './Textarea';
-export type { CollapsiblePanelProps } from './CollapsiblePanel';
