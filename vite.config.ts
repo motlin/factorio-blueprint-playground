e280a071e2f8e144ef1791feeb7dd1243179684c
Craig P. Motlin <cmotlin@gmail.com>
2024-12-01 10:55:05 -0500

    Make it configurable whether the vite rollup-plugin-visualizer plugin runs.

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,37 @@
 import preact from '@preact/preset-vite';
 import {TanStackRouterVite} from '@tanstack/router-plugin/vite';
 import {visualizer} from 'rollup-plugin-visualizer';
-import {defineConfig} from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 
-export default defineConfig({
-    plugins: [
-        preact(),
-        TanStackRouterVite({
-            generatedRouteTree: './src/routeTree.gen.ts',
-            routesDirectory: './src/routes',
-        }),
-        visualizer({
-            open: true,
-            gzipSize: true,
-            brotliSize: true,
-        }),
-    ],
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    'preact': ['preact', '@preact/signals'],
-                    'react-router': ['@tanstack/react-router'],
-                    'fflate': ['fflate'],
-                    'idb-keyval': ['idb-keyval'],
+export default defineConfig(({mode}) => {
+    // Load environment variables
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [
+            preact(),
+            TanStackRouterVite({
+                generatedRouteTree: './src/routeTree.gen.ts',
+                routesDirectory: './src/routes',
+            }),
+            visualizer({
+                open: env.VITE_VISUALIZER_OPEN !== 'false',
+                gzipSize: true,
+                brotliSize: true,
+            }),
+        ],
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        'preact': ['preact', '@preact/signals'],
+                        'react-router': ['@tanstack/react-router'],
+                        'fflate': ['fflate'],
+                        'idb-keyval': ['idb-keyval'],
+                    },
                 },
             },
+            sourcemap: true,
         },
-        sourcemap: true,
-    },
+    };
 });
