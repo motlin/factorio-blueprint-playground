4b25bcc3f24f62de4aa32769611d6d7632678fd1
Craig P. Motlin <cmotlin@gmail.com>
2025-04-21 18:38:29 +0000

    Extract DeconstructionPlannerPanel to its own file.

--- src/components/BlueprintInfoPanels.tsx
+++ src/components/BlueprintInfoPanels.tsx
@@ -1,78 +1,10 @@
 import {memo} from 'react';
 
-import {BlueprintWrapper} from '../parsing/BlueprintWrapper';
-import {BlueprintString, DeconstructionPlanner} from '../parsing/types';
+import {BlueprintString} from '../parsing/types';
 
-import FilterRowsDisplay from './FilterRowsDisplay';
 import {ContentsPanel} from './blueprint/panels/contents/ContentsPanel';
+import {DeconstructionPlannerPanel} from './blueprint/panels/deconstruction/DeconstructionPlannerPanel';
 import {UpgradePlannerPanel} from './blueprint/panels/upgrade/UpgradePlannerPanel';
-import {Cell, Row, Spreadsheet} from './spreadsheet';
-import {Panel} from './ui';
-
-function getFilterModeText(mode?: number): string {
-	if (mode === 1) return 'Banned list: Remove only filtered items';
-	return 'Allowed list: Remove everything except filtered items';
-}
-
-const DeconstructionPlannerPanelComponent = ({blueprint}: {blueprint: BlueprintString}) => {
-	const wrapper = new BlueprintWrapper(blueprint);
-	const {content} = wrapper.getInfo();
-
-	if (!('deconstruction_planner' in blueprint)) return null;
-
-	const {settings} = content as DeconstructionPlanner;
-
-	const getTileSelectionText = (mode?: number) => {
-		switch (mode) {
-			case 1:
-				return 'Default tile behavior';
-			case 2:
-				return 'Never deconstruct tiles';
-			case 3:
-				return 'Always deconstruct tiles';
-			default:
-				return 'Default tile behavior';
-		}
-	};
-
-	return (
-		<Panel title="Deconstruction Settings">
-			<Spreadsheet>
-				{settings.trees_and_rocks_only && (
-					<Row>
-						<Cell width="120px" grow={false}>
-							Mode
-						</Cell>
-						<Cell grow>Only trees and rocks will be marked for deconstruction</Cell>
-					</Row>
-				)}
-				{settings.entity_filter_mode !== undefined && (
-					<Row>
-						<Cell width="120px" grow={false}>
-							Entity Filter Mode
-						</Cell>
-						<Cell grow>{getFilterModeText(settings.entity_filter_mode)}</Cell>
-					</Row>
-				)}
-
-				{settings.entity_filters && (
-					<FilterRowsDisplay filters={settings.entity_filters} type="entity" label="Entity Filters" />
-				)}
-
-				<Row>
-					<Cell width="120px" grow={false}>
-						Tile Selection
-					</Cell>
-					<Cell grow>{getTileSelectionText(settings.tile_selection_mode)}</Cell>
-				</Row>
-
-				{settings.tile_filters && (
-					<FilterRowsDisplay filters={settings.tile_filters} type="tile" label="Tile Filters" />
-				)}
-			</Spreadsheet>
-		</Panel>
-	);
-};
 
 const BlueprintInfoPanelsComponent = ({blueprint}: {blueprint?: BlueprintString}) => {
 	if (!blueprint) return null;
@@ -85,9 +17,6 @@ const BlueprintInfoPanelsComponent = ({blueprint}: {blueprint?: BlueprintString}
 		</>
 	);
 };
-
-DeconstructionPlannerPanelComponent.displayName = 'DeconstructionPlannerPanel';
-export const DeconstructionPlannerPanel = memo(DeconstructionPlannerPanelComponent);
 
 BlueprintInfoPanelsComponent.displayName = 'BlueprintInfoPanels';
 export const BlueprintInfoPanels = memo(BlueprintInfoPanelsComponent);
