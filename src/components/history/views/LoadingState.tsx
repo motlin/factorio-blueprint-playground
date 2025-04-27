500d8dc03c14b659960a92929596e39cd2e86845
Craig P. Motlin <cmotlin@gmail.com>
2025-04-27 03:57:27 +0000

    Extract LoadingState component to history/views directory.

---
+++ src/components/history/views/LoadingState.tsx
@@ -0,0 +1,9 @@
+import {Panel, InsetLight} from '../../ui';
+
+export function LoadingState() {
+	return (
+		<Panel title="Blueprint History">
+			<InsetLight>Loading blueprint history...</InsetLight>
+		</Panel>
+	);
+}
