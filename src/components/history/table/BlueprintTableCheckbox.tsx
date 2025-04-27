edfc1c495159ffe539e63e21a4c353eeec503333
Craig P. Motlin <cmotlin@gmail.com>
2025-04-27 01:35:29 +0000

    Extract BlueprintTableCheckbox to dedicated component.

---
+++ src/components/history/table/BlueprintTableCheckbox.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface BlueprintTableCheckboxProps {
+	isSelected: boolean;
+	onToggle: () => void;
+	onClick?: (e: React.MouseEvent) => void;
+}
+
+export function BlueprintTableCheckbox({isSelected, onToggle, onClick}: BlueprintTableCheckboxProps) {
+	return (
+		<div className="history-checkbox-container">
+			<input
+				type="checkbox"
+				checked={isSelected}
+				onChange={onToggle}
+				onClick={(e) => {
+					e.stopPropagation();
+					onClick?.(e);
+				}}
+				data-testid="blueprint-checkbox"
+			/>
+		</div>
+	);
+}
