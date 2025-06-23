7ecb8300e7a42f619fcff97f5ee9b2cab71d3341
Craig P. Motlin <cmotlin@gmail.com>
2025-06-23 23:21:25 +0000

    Extract BlueprintErrorFallback component to fix nested component definition.

---
+++ src/components/blueprint/error/BlueprintErrorFallback.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {ButtonGreen, InsetDark, Panel} from '../../ui';
+
+interface BlueprintErrorFallbackProps {
+	error: Error;
+	resetErrorBoundary: () => void;
+}
+
+export function BlueprintErrorFallback({error, resetErrorBoundary}: BlueprintErrorFallbackProps) {
+	return (
+		<Panel title="Blueprint Error">
+			<InsetDark>
+				<p>There was an error displaying the blueprint: {error.message}</p>
+				<p>Please try pasting a different blueprint string above.</p>
+				<p />
+				<ButtonGreen onClick={resetErrorBoundary}>Try Again</ButtonGreen>
+			</InsetDark>
+		</Panel>
+	);
+}
